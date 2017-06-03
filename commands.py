6ebae3d6aea03546a0bb45507c3b59516550c323
Gandalf- <austin.voecks@gmail.com>
2017-06-02 17:04:30 -0700

    Reformat both .py files

--- commands.py
+++ commands.py
@@ -11,258 +11,258 @@ bad_zero = 'illegal operation by zero'
 
 ### Helpers
 def not_int(elem):
-  ''' unknown -> boolean
-  '''
-  if isinstance(elem, int):
-    return False
-  else:
-    return True
+    ''' unknown -> boolean
+    '''
+    if isinstance(elem, int):
+        return False
+    else:
+        return True
 
 def get_two_args(stack):
-  ''' list -> int, int || bool, bool
-  '''
-  try:
-    v1 = stack.pop()
-    v2 = stack.pop()
-    return v1, v2
+    ''' list -> int, int || bool, bool
+    '''
+    try:
+        v1 = stack.pop()
+        v2 = stack.pop()
+        return v1, v2
 
-  except (IndexError, TypeError):
-    return False, False
+    except (IndexError, TypeError):
+        return False, False
 
 def pf_error(command, message):
-  ''' string, string -> string, boolean
-  '''
-  return ['runtime error: '+command+': '+message, False]
+    ''' string, string -> string, boolean
+    '''
+    return ['runtime error: '+command+': '+message, False]
 
 
 ### Commands
 def pf_add(stack, command_stack):
-  ''' pops v1,v2, pushes v2 + v1 on stack
+    ''' pops v1,v2, pushes v2 + v1 on stack
 
-  list, list -> list, list
-  '''
-  v1,v2 = get_two_args(stack)
+    list, list -> list, list
+    '''
+    v1,v2 = get_two_args(stack)
 
-  if type(v1) == bool:
-    return pf_error('add',bad_args)
-  if not_int(v1) or not_int(v2):
-    return pf_error('add',bad_int)
+    if type(v1) == bool:
+        return pf_error('add',bad_args)
+    if not_int(v1) or not_int(v2):
+        return pf_error('add',bad_int)
 
-  stack.append(v2 + v1)
-  return stack, command_stack
+    stack.append(v2 + v1)
+    return stack, command_stack
 
 
 def pf_sub(stack, command_stack):
-  ''' pops v1,v2, pushes v2 - v1 on stack
+    ''' pops v1,v2, pushes v2 - v1 on stack
 
-  list, list -> list, list
-  '''
-  v1,v2 = get_two_args(stack)
+    list, list -> list, list
+    '''
+    v1,v2 = get_two_args(stack)
 
-  if type(v1) == bool:
-    return pf_error('sub',bad_args)
-  if not_int(v1) or not_int(v2):
-    return pf_error('sub',bad_int)
+    if type(v1) == bool:
+        return pf_error('sub',bad_args)
+    if not_int(v1) or not_int(v2):
+        return pf_error('sub',bad_int)
 
-  stack.append(v2 - v1)
-  return stack, command_stack
+    stack.append(v2 - v1)
+    return stack, command_stack
 
 
 def pf_mul(stack, command_stack):
-  ''' pops v1,v2, pushes v2 * v1 on stack
+    ''' pops v1,v2, pushes v2 * v1 on stack
 
-  list, list -> list, list
-  '''
-  v1,v2 = get_two_args(stack)
+    list, list -> list, list
+    '''
+    v1,v2 = get_two_args(stack)
 
-  if type(v1) == bool:
-    return pf_error('mul',bad_args)
-  if not_int(v1) or not_int(v2):
-    return pf_error('mul', bad_int)
+    if type(v1) == bool:
+        return pf_error('mul',bad_args)
+    if not_int(v1) or not_int(v2):
+        return pf_error('mul', bad_int)
 
-  stack.append(v2 * v1)
-  return stack, command_stack
+    stack.append(v2 * v1)
+    return stack, command_stack
 
 
 def pf_div(stack, command_stack):
-  ''' pops v1,v2, pushes v2 / v1 on stack
+    ''' pops v1,v2, pushes v2 / v1 on stack
 
-  list, list -> list, list
-  '''
-  v1,v2 = get_two_args(stack)
+    list, list -> list, list
+    '''
+    v1,v2 = get_two_args(stack)
 
-  if type(v1) == bool:
-    return pf_error('div',bad_args)
-  if not_int(v1) or not_int(v2):
-    return pf_error('div',bad_int)
-  if v1 == 0: 
-    return pf_error('div',bad_zero)
+    if type(v1) == bool:
+        return pf_error('div',bad_args)
+    if not_int(v1) or not_int(v2):
+        return pf_error('div',bad_int)
+    if v1 == 0:
+        return pf_error('div',bad_zero)
 
-  stack.append(v2 / v1)
-  return stack, command_stack
+    stack.append(v2 / v1)
+    return stack, command_stack
 
 
 def pf_rem(stack, command_stack):
-  ''' pops v1,v2, pushes v2 * v1 on stack
+    ''' pops v1,v2, pushes v2 * v1 on stack
 
-  list, list -> list, list
-  '''
-  v1,v2 = get_two_args(stack)
-  
-  if type(v1) == bool:
-    return pf_error('rem', bad_args)
-  if not_int(v1) or not_int(v2):
-    return pf_error('rem', bad_int)
-  if v1 == 0:
-    return pf_error('rem', bad_zero)
+    list, list -> list, list
+    '''
+    v1,v2 = get_two_args(stack)
 
-  stack.append(v2 % v1)
-  return stack, command_stack
+    if type(v1) == bool:
+        return pf_error('rem', bad_args)
+    if not_int(v1) or not_int(v2):
+        return pf_error('rem', bad_int)
+    if v1 == 0:
+        return pf_error('rem', bad_zero)
+
+    stack.append(v2 % v1)
+    return stack, command_stack
 
 
 def pf_lt(stack, command_stack):
-  ''' pops v1,v2, pushes result of 'less than' comparison
+    ''' pops v1,v2, pushes result of 'less than' comparison
 
-  list, list -> list, list
-  '''
-  v1,v2 = get_two_args(stack)
+    list, list -> list, list
+    '''
+    v1,v2 = get_two_args(stack)
 
-  if type(v1) == bool:
-    return pf_error('lt', bad_args)
-  if not_int(v1) or not_int(v2):
-    return pf_error('lt', bad_int)
+    if type(v1) == bool:
+        return pf_error('lt', bad_args)
+    if not_int(v1) or not_int(v2):
+        return pf_error('lt', bad_int)
 
-  if v2 < v1:
-    stack.append(1)
-  else:
-    stack.append(0)
-  return stack, command_stack
+    if v2 < v1:
+        stack.append(1)
+    else:
+        stack.append(0)
+    return stack, command_stack
 
 
 def pf_gt(stack, command_stack):
-  ''' pops v1,v2, pushes result of 'greater than' comparison
+    ''' pops v1,v2, pushes result of 'greater than' comparison
 
-  list, list -> list, list
-  '''
-  v1,v2 = get_two_args(stack)
+    list, list -> list, list
+    '''
+    v1,v2 = get_two_args(stack)
 
-  if type(v1) == bool:
-    return pf_error('gt', bad_args)
-  if not_int(v1) or not_int(v2):
-    return pf_error('gt', not_int)
+    if type(v1) == bool:
+        return pf_error('gt', bad_args)
+    if not_int(v1) or not_int(v2):
+        return pf_error('gt', not_int)
 
-  if v2 > v1:
-    stack.append(1)
-  else:
-    stack.append(0)
-  return stack, command_stack
+    if v2 > v1:
+        stack.append(1)
+    else:
+        stack.append(0)
+    return stack, command_stack
 
 
 def pf_eq(stack, command_stack):
-  ''' pops v1,v2, pushes result of 'equal to' comparison
+    ''' pops v1,v2, pushes result of 'equal to' comparison
 
-  list, list -> list, list
-  '''
-  v1,v2 = get_two_args(stack)
+    list, list -> list, list
+    '''
+    v1,v2 = get_two_args(stack)
 
-  if type(v1) == bool:
-    return pf_error('eq', bad_args)
-  if not_int(v1) or not_int(v2):
-    return pf_error('eq', not_int)
+    if type(v1) == bool:
+        return pf_error('eq', bad_args)
+    if not_int(v1) or not_int(v2):
+        return pf_error('eq', not_int)
 
-  if v2 == v1:
-    stack.append(1)
-  else:
-    stack.append(0)
-  return stack, command_stack
+    if v2 == v1:
+        stack.append(1)
+    else:
+        stack.append(0)
+    return stack, command_stack
 
 
 def pf_pop(stack, command_stack):
-  ''' pops v1
+    ''' pops v1
 
-  list, list -> list, list
-  '''
-  try:
-    stack.pop()
-    return stack, command_stack
+    list, list -> list, list
+    '''
+    try:
+        stack.pop()
+        return stack, command_stack
 
-  except IndexError:
-    return pf_error('pop','stack empty')
+    except IndexError:
+        return pf_error('pop','stack empty')
 
 
 def pf_swap(stack, command_stack):
-  ''' pops v1,v2, pushes v1, v2 on the stack.
+    ''' pops v1,v2, pushes v1, v2 on the stack.
 
-  list, list -> list, list
-  '''
-  v1,v2 = get_two_args(stack)
+    list, list -> list, list
+    '''
+    v1,v2 = get_two_args(stack)
 
-  if type(v1) == bool:
-    return pf_error('swap', bad_args)
+    if type(v1) == bool:
+        return pf_error('swap', bad_args)
 
-  stack.append(v1)
-  stack.append(v2)
-  return stack, command_stack
+    stack.append(v1)
+    stack.append(v2)
+    return stack, command_stack
 
 
 def pf_sel(stack, command_stack):
-  ''' pops v1,v2,v3. if v3: push v1 else: push v2
+    ''' pops v1,v2,v3. if v3: push v1 else: push v2
 
-  list, list -> list, list
-  '''
-  try:
-    v1 = stack.pop()
-    v2 = stack.pop()
-    v3 = stack.pop()
+    list, list -> list, list
+    '''
+    try:
+        v1 = stack.pop()
+        v2 = stack.pop()
+        v3 = stack.pop()
 
-  except IndexError:
-    return pf_error('sel', bad_args)
-  if not_int(v3):
-    return pf_error('sel', 'v3 must be int')
+    except IndexError:
+        return pf_error('sel', bad_args)
+    if not_int(v3):
+        return pf_error('sel', 'v3 must be int')
 
-  if v3 == 0:
-    stack.append(v1)
-  else:
-    stack.append(v2)
-  return stack, command_stack
+    if v3 == 0:
+        stack.append(v1)
+    else:
+        stack.append(v2)
+    return stack, command_stack
 
 
 def pf_nget(stack, command_stack):
-  ''' pops v1, pushes the stack element at that position on the stack
+    ''' pops v1, pushes the stack element at that position on the stack
 
-  list, list -> list, list
-  '''
-  try:
-    v1 = stack.pop()
+    list, list -> list, list
+    '''
+    try:
+        v1 = stack.pop()
 
-  except IndexError:
-    return pf_error('nget', bad_args)
-  if not_int(v1):
-    return pf_error('nget', bad_int)
+    except IndexError:
+        return pf_error('nget', bad_args)
+    if not_int(v1):
+        return pf_error('nget', bad_int)
 
-  try:
-    stack.append(stack[len(stack) - v1])
-  except IndexError:
-    return pf_error('nget', 'bad index')
-
-  return stack, command_stack
-
-
-def pf_exec(stack, command_stack):
-  ''' pops v1, prepends it to the command stack
-
-  list, list -> list, list
-  '''
-  try:
-    v1 = stack.pop()
-
-    if type(v1) != list:
-      return pf_error('exec', 'arg is not executable sequence')
-
-    v1.reverse()
-    list(map(command_stack.append, v1))
+    try:
+        stack.append(stack[len(stack) - v1])
+    except IndexError:
+        return pf_error('nget', 'bad index')
 
     return stack, command_stack
 
-  except IndexError:
-    return pf_error('exec', bad_args)
+
+def pf_exec(stack, command_stack):
+    ''' pops v1, prepends it to the command stack
+
+    list, list -> list, list
+    '''
+    try:
+        v1 = stack.pop()
+
+        if type(v1) != list:
+            return pf_error('exec', 'arg is not executable sequence')
+
+        v1.reverse()
+        list(map(command_stack.append, v1))
+
+        return stack, command_stack
+
+    except IndexError:
+        return pf_error('exec', bad_args)
--- interpreter.py
+++ interpreter.py
@@ -9,14 +9,14 @@ import commands
 valid_commands = [ 'sub','add','mul','div','rem', 'lt','gt',
                    'eq','pop','swap', 'sel', 'nget','exec']
 parens = [')','(']
-interactive = True
+interactive = False
 failure = [False, 0]
 
 ### Helpers
 def is_int(string):
     ''' string -> boolean
     '''
-    try: 
+    try:
         isinstance(int(string), int)
         return True
     except (ValueError, TypeError):
@@ -36,7 +36,7 @@ def convert_int(string):
 ### Logic
 def check_program_syntax(program):
     ''' Takes a postfix program as a string, verifies that it has the correct syntax
-    and returns a boolean representing whether it's valid. If it is valid, it also 
+    and returns a boolean representing whether it's valid. If it is valid, it also
     returns an int which is the number of required args
 
     string -> boolean, int
@@ -54,8 +54,8 @@ def check_program_syntax(program):
 
         print('syntax error: bad header')
         return failure
-    
-    # check if all tokens are valid 
+
+    # check if all tokens are valid
     for token in token_list[2:]:
         if not token in valid_commands and not token in parens and not is_int(token):
             print('syntax error: bad command', token)
@@ -118,17 +118,19 @@ def tokenize(token_list, index):
 def run_program(program, arguments):
     ''' string, string -> string
     '''
-    
+
     prog = program.replace('(', '( ').replace(')', ' )').split()
-    prog = list(map(convert_int, prog))
-    prog, index = tokenize(prog, 0)
-    
+    prog = [convert_int(x) for x in prog]
+    prog, _ = tokenize(prog, 0)
+
     # put args on stack
-    args = list(map(int, arguments.split()))
+    args = [int(x) for x in arguments.split()]
     args.reverse()
 
     stack = []
-    if args: list(map(stack.append, args))
+    if args:
+        for x in args:
+            stack.append(x)
 
     # build the command stack
     command_stack = prog[2:]
@@ -157,9 +159,9 @@ def run_program(program, arguments):
         print('commands', command_stack)
         print('')
         command_stack.reverse()
-        
+
         token = command_stack.pop()
-        
+
         # push integer onto the stack
         if is_int(token):
             print('pushing ', token)
@@ -173,7 +175,7 @@ def run_program(program, arguments):
             # stack may contain an error message if there was a problem
             if isinstance(stack, str):
                 return stack
-        
+
         # push command sequence onto the stack
         else:
             print('pushing ', token)
@@ -220,14 +222,14 @@ def main():
                 print('output  ', output)
 
     else:
-        prog1 = '(postfix 0 1 2 add 5 add)'
-        prog2 = '(postfix 0 5 0 div)'
-        prog3 = '(postfix 0 3 4 add 5 mul 6 sub 7 div)'
-        prog4 = '(postfix 0 (3 4) 4 add 5 mul 6 sub 7 div)'
-        prog5 = '(postfix 4 lt (add) (mul) sel exec)'
-        prog6 = '(postfix 1 1 nget 0 lt (0 swap sub) () sel exec)'
+        #prog1 = '(postfix 0 1 2 add 5 add)'
+        #prog2 = '(postfix 0 5 0 div)'
+        #prog3 = '(postfix 0 3 4 add 5 mul 6 sub 7 div)'
+        #prog4 = '(postfix 0 (3 4) 4 add 5 mul 6 sub 7 div)'
+        #prog5 = '(postfix 4 lt (add) (mul) sel exec)'
+        #prog6 = '(postfix 1 1 nget 0 lt (0 swap sub) () sel exec)'
         prog7 = '(postfix 1 ((3 nget swap exec) (2 mul swap exec) swap) (5 sub) swap exec exec)'
-        print(prog6)
+        print(prog7)
         output = run_program(prog7, '-7')
         print(output)
 
@@ -240,6 +242,6 @@ if __name__ == '__main__':
         import __builtin__
         input = getattr(__builtin__, 'raw_input')
     except (ImportError, AttributeError):
-         pass
+        pass
 
     main()
